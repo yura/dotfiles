464c600950431ed10b12c404d3a56dd15cb38bdb
Yury Kotlyarov <yura@brainhouse.ru>
2015-11-21 12:11:27 +0600

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script creates symlinks for all files from current dir to home dir
+#
+
+dir=`pwd`
+olddir=~/dotfiles.old
+files=`ls -I "README*" -I "*.sh"`
+
+echo "Creating backup dir $olddir"
+mkdir -p $olddir 
+
+for file in $files; do
+  if [ -f ~/$file ]; then
+    mv ~/.$file $olddir
+  fi
+
+  echo "Symlinking $file in home directory"
+  ln -s $dir/$file ~/.$file
+done
